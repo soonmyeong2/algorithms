f573f6527f8e7c3cb8fd7f26bd3b74e737e9c25e
unknown <yunujang@naver.com>
2020-12-31 02:43:55 +0900

    https://leetcode.com/problems/letter-combinations-of-a-phone-number/

---
+++ 17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,9 @@
+from itertools import product
+
+
+class Solution(object):
+    def letterCombinations(self, digits):
+        if not digits:
+            return []
+        mapping = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        return map("".join, product(*[mapping[digit] for digit in digits]))
